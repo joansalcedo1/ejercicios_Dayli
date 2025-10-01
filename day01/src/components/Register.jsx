da97f67ebf4ef926156d700ea040d06ff142686e
joansalcedo1 <sebastiansalcedo7@gmail.com>
2025-10-01 16:12:52 -0500

    login y register front

--- day01/src/components/Register.jsx
+++ day01/src/components/Register.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-function Login() {
+function Register() {
     const [typePass, setTypePass] = useState("password");
 
     function handlePassword() {
@@ -12,10 +12,11 @@ function Login() {
     }
     return (
         <>
-            
+            <div className="flex items-end justify-center">
+            </div>
             <div className="flex flex-row items-center text-white justify-center">
                 <div id="log_father" className="pt-5 px-15 bg-gray-700 rounded-4xl">
-                    <h2 className="text-2xl font-bold pb-10">Log in </h2>
+                    <h2 className="text-2xl font-bold pb-10">Register </h2>
                     <div className="grid">
                         <form>
                             <div className=" py-2">
@@ -39,18 +40,27 @@ function Login() {
                                     </button>
                                 </span>
                             </div>
+                            <div className="grid grid-cols-1 ">
+                                <p>Repeat password</p>
+                                <span className="flex items-center gap-3">
+                                    <input
+                                        type={typePass}
+                                        placeholder="Ingresa tu contraseña"
+                                        className="w-full p-2 mb-3 rounded border border-gray-300"
+                                    />
+                                    <button
+                                        type="button"
+                                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+                                        onClick={handlePassword}
+                                    >
+                                        ver
+                                    </button>
+                                </span>
+                            </div>
                             <div>
-                                <button type="submit" className="bg-red-400 text-white hover:bg-red-600 w-full h-10 hover:cursor-pointer">Log in</button>
+                                <button type="submit" className="bg-red-400 text-white hover:bg-red-600 w-full h-10 hover:cursor-pointer">Register in</button>
                             </div>
                         </form>
-                        <div className="text-center py-5">
-                            <p>or</p>
-                            <div className="flex my-2">
-                                <button className="w-full mx-2 bg-green-400 hover:bg-green-600 hover:cursor-pointer">G</button>
-                                <button className="w-full mx-2 bg-yellow-400 hover:bg-yellow-600 hover:cursor-pointer">M</button>
-                                <button className="w-full mx-2 bg-black text-white hover:bg-gray-800 hover:cursor-pointer">A</button>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -58,4 +68,4 @@ function Login() {
     );
 }
 
-export default Login;
+export default Register;
--- day01/src/features/Login_logic.jsx
+++ day01/src/features/Login_logic.jsx
@@ -1,18 +1,15 @@
 import { useState, useEffect } from "react";
+import Login from "../components/Login";
+import Register from "../components/Register";
 function Login_logic() {
     const [estado, setEstado] = useState("cargando");
-    const [typePass, setTypePass] = useState("password");
-
-    function handlePassword() {
-        if (typePass == "password") {
-            setTypePass("text");
-        } else {
-            setTypePass("password");
-        }
+    
+    function changeLogin() {
+        setEstado("login")
     }
-    useEffect(() => {
-
-    }, []);
+    function changeRegister() {
+        setEstado("register")
+    }
     if (estado == "cargando") {
         setEstado("login")
         return (
@@ -25,52 +22,24 @@ function Login_logic() {
     if (estado == "login") {
         return (
             <>
-                <div className="grid grid-cols-2 h-screen w-full bg-gray-100">
+                <div className="grid grid-cols-2 h-screen w-full bg-gray-100 items-center">
                     <div className="flex items-center p-4">
                         <h1 className="text-6xl font-bold mr-4 ">Mi Título</h1>
                     </div>
-                    
-                    <div className="flex flex-row items-center justify-center">
-                        <div className="bg-purple-300">
-                            <p>Log in</p>
-                            <p>register</p>
+                    <div className="">
+                        <div className="flex items-end justify-center">
+                            <div className="bg-amber-900 border rounded-4xl">
+                                <button
+                                    onClick={changeLogin}
+                                    className="w-20 h-10 mx-10 bg-green-400 hover:bg-green-600 hover:cursor-pointer">Log in
+                                </button>
+                                <button
+                                    onClick={changeRegister}
+                                    className="w-20 h-10 mx-10 bg-green-400 hover:bg-green-600 hover:cursor-pointer">register
+                                </button>
+                            </div>
                         </div>
-                        <div id="log_father" className="bg-amber-300 ">
-                            <h2 className="text-2xl font-bold">Log in </h2>
-                            <span>
-                                <p>Email</p>
-                                <input type="text" placeholder="Ingresa tu email" className="w-full p-2 mb-3 rounded border border-gray-300" />
-                            </span>
-                            <span className="">
-                                <p>Password</p>
-                                <span className="flex items-center gap-3">
-
-                                    <input
-                                        type={typePass}
-                                        placeholder="Ingresa tu contraseña"
-                                        className="w-full p-2 mb-3 rounded border border-gray-300"
-                                    />
-                                    <button
-                                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
-                                        onClick={handlePassword}
-                                    >
-                                        ver
-                                    </button>
-                                </span>
-                            </span>
-                            <span className="mt-5">
-                                <button className="bg-red-400 text-white hover:bg-red-600 w-full hover:cursor-pointer">Log in</button>
-                            </span>
-                            <span className="text-center">
-                                <p>or</p>
-                                <span className="flex">
-                                    <button className="w-full mx-2 bg-green-400 hover:bg-green-600 hover:cursor-pointer">G</button>
-                                    <button className="w-full mx-2 bg-yellow-400 hover:bg-yellow-600 hover:cursor-pointer">M</button>
-                                    <button className="w-full mx-2 bg-black text-white hover:bg-gray-800 hover:cursor-pointer">A</button>
-                                </span>
-
-                            </span>
-                        </div>
+                        <Login />
                     </div>
                 </div>
             </>
@@ -80,6 +49,26 @@ function Login_logic() {
     if (estado == "register")
         return (
             <>
+            <div className="grid grid-cols-2 h-screen w-full bg-gray-100 items-center">
+                    <div className="flex items-center p-4">
+                        <h1 className="text-6xl font-bold mr-4 ">Mi Título</h1>
+                    </div>
+                    <div className="">
+                        <div className="flex items-end justify-center">
+                            <div className="bg-amber-900 border rounded-4xl">
+                                <button
+                                    onClick={changeLogin}
+                                    className={`w-20 h-10 mx-10  bg-green-400 hover:bg-green-600 hover:cursor-pointer`}>Log in
+                                </button>
+                                <button
+                                    onClick={changeRegister}
+                                    className="w-20 h-10 mx-10 bg-green-400 hover:bg-green-600 hover:cursor-pointer">register
+                                </button>
+                            </div>
+                        </div>
+                        <Register />
+                    </div>
+                </div>
             </>
         );
 }
